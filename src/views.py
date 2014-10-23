347255e9d943417a7e8c5bbc71d42b579a752601
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2014-10-23 22:38:25 +0400

    update button

--- src/views.py
+++ src/views.py
@@ -4,9 +4,9 @@ from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.shortcuts import redirect, render_to_response
 from django.template import RequestContext, TemplateDoesNotExist, Context as StdContext
 from django.core.urlresolvers import reverse
-from django import forms
+from django.conf import settings
 
-import data
+import data, papi, settings
 # https://ru.wargaming.net/developers/api_explorer/wot/clan/info/?application_id=demo&fields=members.account_id,members.account_name&clan_id=43856&http_method=GET&run=1
 
 def mainpage(request):
@@ -22,7 +22,16 @@ def clan(request, clanid):
     return render_to_response('view.html', {'clanid': clanid, 'clantag': data.clans[clanid][0]})
 
 def edit(request, clanid):
-  return HttpResponse("edit '%s'" % clanid)
+    if clanid not in data.clans:
+        raise(Http404)
+
+    if request.method == 'POST':
+        if "update" in request.POST:
+            dat = papi.Session(papi.Server.RU, settings.papy_key).fetch('wot/clan/info', 'fields=members.account_name&clan_id=%s' % clanid)
+            logging.warning("update: %s" % repr(dat))
+            return redirect(reverse('clan', None, [], {'clanid': clanid,}))
+
+    return render_to_response('edit.html', RequestContext(request, {'clantag': data.clans[clanid][0]}))
 
 from google.appengine.ext import db
 
